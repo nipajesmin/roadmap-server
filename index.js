03e18710312cbae36db09c7283b0951b56d97b12
nipajesmin <nipakterjesmin@gmail.com>
2025-06-22 13:10:49 +0600

    changed in index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3100;
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 //middleware
@@ -11,15 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 
-// app.get('/', (req, res) => {
-//   res.send('server running');
-// })
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgk5l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,14 +25,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    //await client.db("admin").command({ ping: 1 });
-    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-
-    const database = client.db('roadmapDB');
+   
     const userCollection = client.db('roadmapDB').collection('users');
     const roadmapItemCollection = client.db('roadmapDB').collection('roadmapItems');
 
@@ -51,23 +38,11 @@ async function run() {
       res.send(result);
     })
 
-    // app.get('/users', async (req, res) => {
-    //   const cursor = userCollection.find();
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // })
-
     app.get('/users', async (req, res) => {
-      try {
-        const cursor = userCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-      } catch (err) {
-        console.error('Error fetching users:', err);
-        res.status(500).send({ message: 'Server error' });
-      }
-    });
-
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
 
     app.get('/roadmapItems', async (req, res) => {
       const cursor = roadmapItemCollection.find();
@@ -223,9 +198,8 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get("/", (req, res) => {
-  res.send("Marriage BD Server is running");
+app.use("/", (req, res) => {
+  res.send("Roadmap BD Server is running");
 });
 
 app.listen(port, (req, res) => {
